f71d64eb33d31717a1ebbaaee9be72500a777193
sirifox <45976344+sirifox@users.noreply.github.com>
2019-02-02 17:18:03 +0300

    Update 2.1.files.py

--- 2.1.files.py
+++ 2.1.files.py
@@ -1,24 +1,23 @@
-cook_book = {}
+def cookbook_read():
+  with open('cookbook.txt') as file:
+    lines = file.readlines()
+  cook_book = {}
+  for num, line in enumerate(lines):
+    if line == '\n':
+      continue
+    elif num == 0 or lines[num-1] == '\n':
+      ingridients = []
+      for ingr in lines[num+2:num+2+int(lines[num+1])]:
+        ingr = ingr.split(' | ')
+        ingridients.append({'ingridient_name': ingr[0], 'quantity': int(ingr[1]), 'measure': ingr[2].strip()})
+      cook_book[line.strip()] = ingridients
+  del lines
+  return cook_book
 
-with open('cookbook.txt') as file:
-  lines = file.readlines()
+# print('\n', 'Задание_1:', '\n', '\n', cookbook_read(), '\n', sep = '')
 
-for num, line in enumerate(lines):
-  if line == '\n':
-    continue
-  elif num == 0 or lines[num-1] == '\n':
-    ingridients = []
-    for ingr in lines[num+2:num+2+int(lines[num+1])]:
-      ingr = ingr.split(' | ')
-      ingridients.append({'ingridient_name': ingr[0], 'quantity': int(ingr[1]), 'measure': ingr[2].strip()})
-    cook_book[line.strip()] = ingridients
-
-del lines
-
-print('\n', 'Задание_1:', '\n', sep = '')
-print(cook_book, '\n', sep = '')
-
-def get_shop_list_by_dishes(dishes, person_count):
+def get_shop_list_by_dishes(cook_book, person_count):
+  dishes = list(cook_book.keys())
   ingridients_dict = {}
   for dish in dishes:
     if dish in list(cook_book.keys()):
@@ -30,5 +29,4 @@ def get_shop_list_by_dishes(dishes, person_count):
           ingridients_dict[ingr['ingridient_name']]['quantity'] += ingr['quantity'] * person_count
   return ingridients_dict
 
-print('Задание_2:', '\n', sep = '')
-print(get_shop_list_by_dishes(list(cook_book.keys()), 2))
+print('Задание_2:', '\n', '\n', get_shop_list_by_dishes(cookbook_read(), 2), sep = '')
